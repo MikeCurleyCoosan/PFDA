8b4a0c17394691fd9742c5421e77c04f860c465d
Michael Curley <118756697+MikeCurleyCoosan@users.noreply.github.com>
2024-11-24 12:49:20 +0000

    Addign messing_weather.ipynb

---
+++ PFDA_mywork/PFDA_Week6/Risk/battle.py
@@ -0,0 +1,177 @@
+class Battle:
+    #This class will simulate a battle between two armies. The battle will be between an attacker and a defender
+    #The battle will have the following attributes:
+    #1. The number of attacking armies
+    #2. The number of attacking dice (max 3)
+    #3. The number of defending armies
+    #4. The number of defending dice (max 2)
+    #The battle will involve the smallest number of armies put forward by either the attacker or defender
+    #The battle can be of infinite army size but the number of dice will be limited to 3 for the attacker and 2 for the defender
+
+    #Author: Michael Curley
+    #Date: 24/10/2024
+
+    #import required classes
+    from attacker import Attacker as attacker
+    from defender import Defender as defender
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    
+    def sub_menu():
+        #Request army and dice information from user
+        attack_armies = int(input("\t Please enter the number of attacking armies "))
+        attack_dice = int(input("\t Please enter the number of attacking soldiers in each army "))
+        #Check that the number of attacking dice is less than 3
+        while attack_dice < 0 or attack_dice > 3:
+            print("The number of attacking dice must be between 1 and 3")
+            attack_dice = int(input("\t Please enter the number of attacking soldiers in each army "))
+        defence_armies = int(input("\t Please enter the number of defending armies "))
+        defending_dice = int(input("\t Please enter the number of defending soldiers in each army "))
+        #Check that the number of defending dice is less than 2
+        while defending_dice < 0 or defending_dice > 2:
+            print("The number of defending dice must be between 1 and 2")
+            defending_dice = int(input("\t Please enter the number of defending soldiers in each army "))
+        return attack_armies, attack_dice, defence_armies, defending_dice
+    
+    
+    attack_armies, attack_dice, defence_armies, defending_dice = sub_menu()
+    
+    #Battles will take the least number of armies put forward by either attack or defence
+    if attack_armies < defence_armies:
+        defence_armies = attack_armies
+    else:
+        attack_armies = defence_armies
+
+    #Create an instance of an offence
+    offence = attacker(attack_armies, attack_dice)
+    #Create an instance of an defence
+    defence = defender(defence_armies, defending_dice)
+
+    #Roll the dice for the offence. This will create an numpy array to store the dice rolls for the attack
+    offence_dice = offence.roll_dice()
+    #Roll the dice for the defence. This will create an numpy array to store the dice rolls for the attack
+    defence_dice = defence.roll_dice()
+
+    #RESULTS OF THE BATTLE
+    print('\n')
+    print('************************************************************************************************')
+    print('\t\tRESULTS OF THE BATTLE')
+    print('************************************************************************************************')
+    print('\n')
+
+    #The number of attacking armies put into the battle
+    print(f'The number of attacking soldiers put into the battle was : {attack_armies*attack_dice}')
+
+    #The number of defending armies put into the battle
+    print(f'The number of defending soldiers put into the battle was : {defence_armies*defending_dice}')
+
+
+    #Calculate the number of armies lost by the attacker
+    offence_armies_lost = offence.calculate_attacker_armies_lost(attack_armies, offence_dice, defence_dice)
+    print(f'The number of attaking soldiers lost in this battle was : {offence_armies_lost}')
+
+
+    #Calculate the number of armies lost by the defender
+    defence_armies_lost = defence.calculate_defender_armies_lost(defence_armies, offence_dice, defence_dice)
+    print(f'The number of defending soldiers lost in this battle was : {defence_armies_lost}')
+
+    #Calculate the number of armies remaining for the attacker
+    offence_armies_remaining = offence.armies_remaining_attacker(attack_armies, attack_dice, offence_armies_lost)
+    print(f'The number of attaking soldiers remaining in this battle was : {offence_armies_remaining}')
+
+    #Calculate the number of armies remaining for the defender
+    defence_armies_remaining = defence.armies_remaining_defender(defence_armies, defending_dice, defence_armies_lost)
+    print(f'The number of defending soldiers remaining in this battle was : {defence_armies_remaining}')
+
+    print('\n')
+    print('************************************************************************************************')
+
+    #Testing 
+    #print("The attacking army dice rolls are ")
+    #print(offence_dice
+    #print("The defending army dice rolls are ")
+    #print(defence_dice)
+
+    
+    # Create a new matrix to store the results of the battles
+    results = np.zeros((attack_armies, len(defence_dice[1])))
+
+    # Loop through the dice rolls and compare the results
+    for i in range(attack_armies):
+       for j in range(len(defence_dice[1])):
+            if defence_dice[i, j] == offence_dice[i, j]:
+               results[i, 1] += 1
+            elif defence_dice[i, j] > offence_dice[i, j]:
+                results[i, 1] += 1
+            else:
+               results[i, 0] += 1
+
+    #Print the results of the battle 
+    #print(results)
+
+    #Convert the results from a numpy float array to an integer array
+
+    results = results.astype(int)
+    results
+
+    # Create a pandas dataframe to store the results
+
+    df = pd.DataFrame(results, columns=['Attacker Wins', 'Defender Wins'])
+    print(df)
+
+
+    # Percentage of wins for the 
+    attacker_percentage = 0
+    for i in range (attack_armies):
+        if df['Attacker Wins'][i] == 2:
+            attacker_percentage += 1/(attack_armies)
+    #print(attacker_percentage)
+
+
+    # Percentage of wins for the defender   
+    defender_percentage = 0
+    for i in range (attack_armies):
+        if df['Defender Wins'][i] == 2:
+            defender_percentage += 1/(attack_armies)
+    #print(defender_percentage)
+
+    # Draw percentage
+    draw_percentage = 1 - attacker_percentage - defender_percentage
+    #print(draw_percentage)
+
+    # Double check the percentages
+    draw_percentage_check=0
+    for i in range (attack_armies):
+        if df['Attacker Wins'][i] == 1 and df['Defender Wins'][i] == 1:
+            draw_percentage_check += 1/(attack_armies)
+
+    #print(draw_percentage_check)
+
+    # Plot the results as a pie chart
+
+    fig, ax = plt.subplots()
+
+    #Set the font style and size for the title and labels
+    font1 = {'family':'serif','color':'blue','size':20}
+    font2 = {'family':'serif','color':'darkred','size':15}
+
+    #Set the title of the pie chart
+    ax.set_title('Risk Game Results', fontdict = font1)
+
+    labels= 'Attacker Wins', 'Defender Wins', 'Draw'
+    sizes = [attacker_percentage, defender_percentage, draw_percentage]
+    colors = ['blue', 'red', 'green']
+    explode = (0.1, 0.1, 0.1)  # explode the slices 
+
+    ax.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+    ax.set_xlabel('Percentage of Wins', fontdict = font2)
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax.axis('equal')
+
+    #Create a pie chart of the results in the plots folder
+    plt.savefig("Plots/battle.png")
+    plt.clf()
+    plt.close()
+
---
+++ PFDA_mywork/PFDA_Week6/Risk/menu.py
@@ -0,0 +1,41 @@
+class Menu:
+#Author: Michael Curley
+#Date: 24/10/2024
+#This class will be used to display a menu to the user, and will run the appropriate code based on the user's choice
+    
+    #Method to display the menu to the user
+    def display_menu():
+        print("\t Risk Game Menu")
+        print("\t(a). Simulate a battle")
+        print("\t(b). Simulate a war")
+        print("\t(Q). Exit")
+
+        choice = input("Type one letter (a,b or q):").strip().lower()
+        return choice
+    
+    #Method to simulate a battle
+    def simulate_battle():
+       print("\t Create a battle simulation")
+       from battle1000 import Battle1000 as battle1000
+       battle1000()
+       print("\t Battle simulation complete")
+       
+
+    #Method to simulate a war
+    def simulate_war():
+        print("\t Create a war simulation")
+        from battle import Battle as battle
+        battle()
+
+    #Dummy method to exit the program nicely
+    def do_nothing():
+        pass
+
+    #Dictionary to map the user's choice to the appropriate method
+    choice_map = {
+        "a": simulate_battle,
+        "b": simulate_war,
+        "q": do_nothing
+    }
+
+    
