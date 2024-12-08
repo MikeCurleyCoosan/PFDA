54b8d50ab565e85412b4126a4045118380582f07
Michael Curley <michaelcurley1@gmail.com>
2024-12-08 23:53:42 +0000

    Updating windfarm2.ipynb

--- PFDA_Project/python/clean_dataset.py
+++ PFDA_Project/python/clean_dataset.py
@@ -12,14 +12,16 @@ class CleanDataset:
 
 
     #Constructor. This is the code that is run when a new instance of the class is created.
-    def __init__(self, filename, skiprows):
+    def __init__(self, filename, id, skiprows):
         import pandas as pd
-        file_path = './../data/hourly_data'
+        file_path = 'data/hourly_data/'
         self.filename = filename
         self.skiprows = skiprows
-        url = file_path + filename
+        self.id = id
+        location = ('{}'+ '{}' + '_' + '{}'+'.csv').format(file_path, filename, id)
+        #print(location)
         try:
-            self.dataset = pd.read_csv(url, 
+            self.dataset = pd.read_csv(location, 
                                    skiprows=skiprows, 
                                    skipinitialspace=True,
                                    na_values = ['-', 'N/A', 'NA', 'nan', 'NaN', 'NAN', 'Nan', 'N/a', 'n/a', 'na', ''] 
@@ -32,13 +34,13 @@ class CleanDataset:
         if self.skiprows == 23:
             try:
                 self.dataset.columns = ['Date/Time (utc)', 'Indicator', 'Precipitation Amount (mm)', 'Indicator', 'Temperature (°C)', 'Indicator', 'Wet Bulb Temperature (°C)', 'Dew Point Temp (°C)', 'Vapour Pressure (hPa)','Relative Humidity (%)', 'Mean Sea Level Pressure (hPa)','Indicator', 'Mean Wind Speed (knot)', 'Indicator', 'Predominant Wind Direction (deg)', 'Present Weather', 'Past Weather', 'Sunshine duration (hours)', 'Visibility', 'Cloud Height (ft * 100s)', 'Cloud amount']
-                print('Dataset imported')
+                #print('Dataset imported')
             except KeyError:
                 print('Columns not found. Please check the column names')
         else:
             try:
                 self.dataset.columns = ['Date/Time (utc)', 'Indicator', 'Precipitation Amount (mm)', 'Indicator', 'Temperature (°C)', 'Indicator', 'Wet Bulb Temperature (°C)', 'Dew Point Temp (°C)', 'Vapour Pressure (hPa)','Relative Humidity (%)', 'Mean Sea Level Pressure (hPa)','Indicator', 'Mean Wind Speed (knot)', 'Indicator', 'Predominant Wind Direction (deg)']
-                print('Dataset imported')
+                #print('Dataset imported')
             except KeyError:
                 print('Columns not found. Please check the column names')
         
@@ -47,10 +49,10 @@ class CleanDataset:
     #Create a function to remove the NA values from the dataset
     def remove_na_values(self):
         import pandas as pd
-        print('Removing NA values')
+        #print('Removing NA values')
         #Remove the indicator columns
         self.dataset.drop(columns=['Indicator'], inplace=True)
-        print('Indicator columns removed')
+        #print('Indicator columns removed')
         #Remove the rows with NA values
         self.dataset.dropna(inplace=True, ignore_index=True)
 
@@ -62,21 +64,23 @@ class CleanDataset:
         self.dataset['Date/Time (utc)'] = pd.to_datetime(self.dataset['Date/Time (utc)'], format='%d-%b-%Y %H:%M')
         self.dataset['Month'] = self.dataset['Date/Time (utc)'].dt.month
         self.dataset['Year'] = self.dataset['Date/Time (utc)'].dt.year
-        print('Datetime cleaned')
+        #print('Datetime cleaned')
 
     #Create a function to rename the wind column and reduce the dataset to only the columns we need
     def rename_windcolumn(self):
         import re
-        station_name = re.findall(r'_(.*?)_', self.filename)
+        station_name = self.filename.split('_')
+        #print(station_name)
         name = str(station_name[0]) + '_Wind_Speed'
         name = self.dataset[['Date/Time (utc)', 'Mean Wind Speed (knot)', 'Month', 'Year']].copy()
         name.rename(columns={'Mean Wind Speed (knot)': 'Mean Wind Speed (knot)_' + str(station_name[0])}, inplace=True)
         self.dataset = name
-        print('Wind column renamed')
-        print(self.dataset.head())
+        #print('Wind column renamed')
+        #print(self.dataset.head())
 
     def clean_dataset(self):
         self.remove_na_values()
         self.clean_datetime()
         self.rename_windcolumn()
+        
         return self.dataset
