098263002964ee0e33bda95308aa2a5ed78133f1
Michael Curley <michaelcurley1@gmail.com>
2024-12-28 19:17:08 +0000

    Adding best fit class to PFDA_Project/python

--- PFDA_Project/python/best_fit.py
+++ PFDA_Project/python/best_fit.py
@@ -3,59 +3,70 @@ class BestFit:
 #This class is used to create a best fit line when comparing the mean windspeed over an extended period of time
 #https://numpy.org/doc/stable/reference/generated/numpy.polyfit.html#numpy-polyfit
 #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_numpy.html#pandas.DataFrame.to_numpy
-
-
     #Constructor. This is the code that is run when a new instance of the class is created. Here we pass in the dataframe
     def __init__(self, df): #The constructor takes in the dataframe
         self.df = df
 
     #Create a function to create a best fit line for the petal length and petal width variables
-    def best_fit(self):
+    def best_fit(self, placename):
         #Import the required libraries
         import numpy as np
         import matplotlib.pyplot as plt
         import matplotlib.dates as mdates
 
-        #Create local variables for the petal length and petal width variables
-        mean_wind= self.df['Mean Wind Speed (knot)']
-        #Convert the petal length variable to a numpy array
-        plen = plen.to_numpy()
-        #Create a local variable for the petal width variable
-        pwdith = self.df['petal_width']
-        #Convert the petal width variable to a numpy array
-        pwdith = pwdith.to_numpy()
+        #Create a placeneame variable to store the placename
+        placename = ''
+        #Set the placename variable to the placename passed in
+        self.placename = placename
+        #Create a variable to store the column name for the mean wind speed
+        windspeed = 'Mean Wind Speed (knot)_' + placename
 
-        #Set the font for the x and y axis labels and the title 
+        bestfit = self.df[['Date/Time (utc)', windspeed]]
+
+        #Convert the Mean Wind Speed (utc) column to m/s
+        bestfit['Mean Wind Speed (m/s)'] = round(bestfit[windspeed] * 0.514444, 2)
+
+        #Resample for monthly wind speed data while keeping the mean and the index as the date
+        bestfit = bestfit.resample('ME', on='Date/Time (utc)').mean()
+
+        #Select data from 2012 onwards
+        bestfit = bestfit[bestfit.index.year >= 2012]
+
+        #Convert the Date/Time (utc) column to a number so that it can be used in the polyfit function
+        bestfit['DateNumber'] = mdates.date2num(bestfit.index)
+
+        #Calculate the best fit parameters for the wind speed data m,c
+        idx = np.isfinite(bestfit['DateNumber']) & np.isfinite(bestfit[windspeed])
+
+        #Calculate our values for m and c
+        m, c = np.polyfit(bestfit['DateNumber'][idx], bestfit[windspeed][idx], 1)
+
+        #Plot the best fit line
+
+        #Set the font for the# x and y axis labels and the title 
         font1 = {'family':'serif','color':'blue','size':20}
         font2 = {'family':'serif','color':'darkred','size':15}
 
         #Create a figure and axes object
         fig, ax = plt.subplots()
-        #Create a scatter plot of the petal length and petal width variables
-        ax.plot(plen, pwdith, 'o', label='Original data', markersize=5) #The 'o' is used to create a scatter plot
-                                                                        #The markersize is used to set the size of the markers
-
-        #We are using the numpy polyfit function to create a best fit line for the petal length and petal width variables
-        #This function returns the coefficients of the best fit line (m and c)
-        #The 1 in the function is used to specify that we want a linear best fit line
-        m, c = np.polyfit(plen, pwdith, 1)
+        #Create a scatter plot of the wind speed data from 2012 onwards
+        ax.plot(bestfit['Mean Wind Speed (m/s)'], 'o', label='Data', color='blue', markersize=5) #The 'o' is used to create a scatter plot
 
         #Create the best fit line using the coefficients returned from the polyfit function
-        #plen is the x-axis variable
-        #m*plen + c is the equation of the line
-        #The 'r' is used to set the colour of the line to red
-        ax.plot(plen, m*plen + c, 'r', label='Best fit line')
+        ax.plot(m*bestfit + c, 'r', label='Best fit line')
 
         #Set the x and y axis labels
-        ax.set_xlabel('Petal Length (cm)', fontdict=font2)
-        ax.set_ylabel('Petal Width (cm)', fontdict=font2)
+        ax.set_xlabel('Year', fontdict=font2)
+        ax.set_ylabel('Mean Wind Speed (m/s)', fontdict=font2)
 
         #Add a title to the plot
-        plt.title('Best fit line for Petal Length and Petal Width', fontdict=font1)
+        plt.title('Best fit line for Wind Speed data from 2012', fontdict=font1)
         #Add a legend
-        plt.legend()
+        plt.legend(loc='upper right', fontsize=12)
+
+        
         #Save the plot as a .png file
-        plt.savefig('Plots/best_fit_line.png')
+        plt.savefig('images/best_fit_line.png')
         #Clear the current figure
         plt.clf()
         #Close the current figure
